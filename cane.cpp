1ae9b3de318fab2f11c8a267ae03d3980154af47
sp1rale <dimayaremchyk24@gmail.com>
2023-11-12 21:33:52 +0200

    Add project files.

---
+++ cane.cpp
@@ -0,0 +1,9 @@
+#include "cane.h"
+
+
+
+
+
+		
+	
+
---
+++ cane.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void showRectangle();
+void showFactorial();
+void showCircle();
+void showSquare();
