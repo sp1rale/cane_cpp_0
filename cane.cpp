30ca2057585f6109028cc471c04e9245e2e69c10
sp1rale <dimayaremchyk24@gmail.com>
2023-11-12 22:08:35 +0200

    d

--- cane.cpp
+++ cane.cpp
@@ -1,6 +1,21 @@
 #include "cane.h"
 
 
+void showRectangle(int height, int width) {
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    showRectangle(5, 7);
+
+    return 0;
+}
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,2 +1,7 @@
 #include "cane.h"
 
+void showRectango(int height, int width) {
+}
+void showFactorial();
+void showCircle();
+void showSquare();
